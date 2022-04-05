818c56a1e9ea50bd4cb0cdf4068ac76197309d29
mramzansabir-rmz <mramzansabir.itsrmz@gmail.com.com>
2022-04-05 10:18:44 +0500

    simple css check list

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './App.css';
+
+function App() {
+  return (
+      <section>
+        <div>
+          <h2>Simple CSS check list</h2>
+          <ul className="check-list">
+            <li>Head</li>
+            <li>Shoulders</li>
+            <li>Knees</li>
+            <li>Toes</li>
+          </ul>
+        </div>
+      </section>
+  );
+}
+
+export default App;
